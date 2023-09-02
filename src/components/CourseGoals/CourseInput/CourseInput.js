d4416e786f3b9ac8de14c175c64e645c15a27d7c
Arunthamizh-laptop <workattwilight@gmail.com>
2023-09-03 00:23:38 +0530

    video 8 completed this  is the final video

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,37 +1,37 @@
 import React, { useState } from 'react';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import styles from './CourseInput.module.css'; // the css is scoped to this component
 import styled from 'styled-components'
 
-const FormControl = styled.div`
+// const FormControl = styled.div`
 
-  margin: 0.5rem 0;
+//   margin: 0.5rem 0;
 
-& label {
-  font-weight: bold;
-  display: block;
-  margin-bottom: 0.5rem;
-  color: ${props => props.invalid ? 'red' : 'black'}
-}
+// & label {
+//   font-weight: bold;
+//   display: block;
+//   margin-bottom: 0.5rem;
+//   color: ${props => props.invalid ? 'red' : 'black'}
+// }
 
-& input {
-  display: block;
-  width: 100%;
-  border: 1px solid ${props => props.invalid ? 'red' : '#ccc'};
-  background: ${props => props.invalid ? '#ffd7d7' : 'transparent' }
-  font: inherit;
-  line-height: 1.5rem;
-  padding: 0 0.25rem;
-}
+// & input {
+//   display: block;
+//   width: 100%;
+//   border: 1px solid ${props => props.invalid ? 'red' : '#ccc'};
+//   background: ${props => props.invalid ? '#ffd7d7' : 'transparent' }
+//   font: inherit;
+//   line-height: 1.5rem;
+//   padding: 0 0.25rem;
+// }
 
-& input:focus {
-  outline: none;
-  background: #fad0ec;
-  border-color: #8b005d;
-}
+// & input:focus {
+//   outline: none;
+//   background: #fad0ec;
+//   border-color: #8b005d;
+// }
 
-`;// taged template literal
+// `;// taged template literal
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -55,14 +55,20 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      {/* <div className="form-control"> */}
-      {/* <FormControl className={!isValid && 'invalid' }> */}
-      <FormControl invalid={!isValid }>
+      {/* access the css module file for style */}
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid }`}>
+
+      {/* below approach is for styled css approach */}
+
+      {/* <FormControl className={!isValid && 'invalid' }>  */} 
+        {/* styled css approach and pass the value as props to styled css */}
+      {/* <FormControl invalid={!isValid }> */}
+
         <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
         <input style={{borderColor: !isValid ? 'red': '#ccc',
                       background: !isValid ? 'salmon': 'transparent' }} type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
-      {/* </div> */}
+      {/* </FormControl> */}
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
