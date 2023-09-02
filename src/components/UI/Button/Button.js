983fc27ab1338f74361d24a12aa228645f69be8f
Arunthamizh-laptop <workattwilight@gmail.com>
2023-09-02 23:58:29 +0530

    video 7 css module class

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,44 +1,46 @@
-// import React from 'react';
+import React from 'react';
 
 import styled from 'styled-components';
+//  css module class approach to set claas to be unique to this component.
+  // Not allow to apply to other component it create a unique call name for the call
+    // Change the css file as Buttom.module.css  
+import styles from './Button.module.css';
 
-// import './Button.css';
-
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 // New way to create button with styled component
 
-const Button = styled.button`
-    width: 100%;
-    font: inherit;
-    padding: 0.5rem 1.5rem;
-    border: 1px solid #8b005d;
-    color: white;
-    background: #8b005d;
-    box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-    cursor: pointer;
-    //  media query with styled components
-    @media (min-width: 768px){
-       width : auto; 
-    }
+// const Button = styled.button`
+//     width: 100%;
+//     font: inherit;
+//     padding: 0.5rem 1.5rem;
+//     border: 1px solid #8b005d;
+//     color: white;
+//     background: #8b005d;
+//     box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//     cursor: pointer;
+//     //  media query with styled components
+//     @media (min-width: 768px){
+//        width : auto; 
+//     }
   
-  .&:focus {
-    outline: none;
-  }
+//   .&:focus {
+//     outline: none;
+//   }
   
-  .&:hover,
-  .&:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
+//   .&:hover,
+//   .&:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
   
-`; // taged template literal
+// `; // taged template literal
 
 export default Button;
